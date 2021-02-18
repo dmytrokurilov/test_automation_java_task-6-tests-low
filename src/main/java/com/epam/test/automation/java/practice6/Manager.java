75392f2f1c381a9080f9e582b4ec236f00fba236
dmytrokurilov <dmytrokurilov@gmail.com>
2021-02-18 19:47:57 +0200

    Task Done

--- src/main/java/com/epam/test/automation/java/practice6/Manager.java
+++ src/main/java/com/epam/test/automation/java/practice6/Manager.java
@@ -1,9 +1,26 @@
 package com.epam.test.automation.java.practice6;
 
-/**
- * <summary>
- * Implement code according to description of task.
- * </summary>
- */
-public class Manager {
+public class Manager extends Employee {
+    private final int quantity;
+
+    public Manager(String name, double salary, int clientAmount)
+    {
+        super(name, salary);
+        quantity = clientAmount;
+    }
+    public void setBonus(double bonus)
+    {
+        if (quantity <= 100)
+        {
+            super.setBonus(bonus);
+        }
+        if (quantity > 100 && quantity <= 150)
+        {
+            super.setBonus(bonus + 500);
+        }
+        if (quantity > 150)
+        {
+            super.setBonus(bonus + 1000);
+        }
+    }
 }
--- src/main/java/com/epam/test/automation/java/practice6/SalesPerson.java
+++ src/main/java/com/epam/test/automation/java/practice6/SalesPerson.java
@@ -1,9 +1,26 @@
 package com.epam.test.automation.java.practice6;
 
-/**
- * <summary>
- * Implement code according to description of task.
- * </summary>
- */
-public class SalesPerson {
+public class SalesPerson extends Employee {
+    private final int percent;
+
+    public SalesPerson(String name, double salary, int percent)
+    {
+        super (name, salary);
+        this.percent = percent;
+    }
+    public void setBonus(double bonus)
+    {
+        if (percent <= 100)
+        {
+            super.setBonus(bonus);
+        }
+        if (percent > 100 && percent <= 200)
+        {
+            super.setBonus(bonus * 2);
+        }
+        if (percent > 200)
+        {
+            super.setBonus(bonus * 3);
+        }
+    }
 }

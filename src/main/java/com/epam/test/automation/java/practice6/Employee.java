4f980465952a1463310c78bebf0d754abd14ccfa
VQCNvwrL <vqcnvwrl@email.com>
2021-02-19 20:42:50 +0000

    Update Employee.java Revert to Task Done 6 through UI

--- src/main/java/com/epam/test/automation/java/practice6/Employee.java
+++ src/main/java/com/epam/test/automation/java/practice6/Employee.java
@@ -2,7 +2,7 @@ package com.epam.test.automation.java.practice6;
 
 import java.math.BigDecimal;
 
-public abstract class Employee {
+public class Employee {
     private final String name;
     private BigDecimal salary;
     private BigDecimal bonus;
@@ -22,7 +22,7 @@ public abstract class Employee {
         this.salary = salary;
     }
 
-    protected Employee(String name, BigDecimal salary) {
+    public Employee(String name, BigDecimal salary) {
         this.name = name;
         this.salary = salary;
     }
@@ -31,14 +31,13 @@ public abstract class Employee {
         return bonus;
     }
 
-    public abstract void setBonus(BigDecimal bonus);
-
+    public void setBonus(BigDecimal bonus) {
+        this.bonus = bonus;
+    }
 
     public BigDecimal toPay() {
 
         return salary.add(bonus);
 
     }
-
-
 }
